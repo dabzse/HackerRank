ecd1caa54f2fce3920faa10d4dea395cfcfbd327
Mihaly Nyilas <dr.dabzse@gmail.com>
2024-01-01 20:03:18 +0100

    add: Python: Basic Data Types: Finding the percentage

---
+++ Python/Basic Data Types/Finding the percentage.py
@@ -0,0 +1,13 @@
+# language: pypy3
+
+from statistics import mean
+
+if __name__ == '__main__':
+    n = int(input())
+    student_marks = {}
+    for _ in range(n):
+        name, *line = input().split()
+        scores = list(map(float, line))
+        student_marks[name] = scores
+    query_name = input()
+    print(format(mean(student_marks[query_name]), ".2f"))
