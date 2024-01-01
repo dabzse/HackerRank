969da38e3729575aa963281db0bbc03e3125cf75
Mihaly Nyilas <dr.dabzse@gmail.com>
2024-01-01 19:48:25 +0100

    add: Python: Introduction: Python If-Else

---
+++ Python/Introduction/Python If-Else.py
@@ -0,0 +1,14 @@
+# language: pypy3
+
+if __name__ == '__main__':
+    n = int(input().strip())
+    if n % 2 == 1 or n == 0:
+        print("Weird")
+
+    if n % 2 == 0 and n in range(2,6):
+        print("Not Weird")
+    elif n % 2 == 0 and n in range(6,21):
+        print("Weird")
+
+    if  n % 2 == 0 and n > 20:
+        print("Not Weird")
