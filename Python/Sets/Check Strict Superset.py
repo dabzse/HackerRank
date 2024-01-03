dd66d3a2c3f88e7791c822024d34cd64c15c21fd
Mihaly Nyilas <dr.dabzse@gmail.com>
2024-01-03 18:41:12 +0100

    add: Python: Sets: Check Strict Superset

---
+++ Python/Sets/Check Strict Superset.py
@@ -0,0 +1,10 @@
+# language: pypy3
+
+A = set(input().split())
+
+for _ in range(int(input())):
+    if not A.issuperset(set(input().split())):
+        print(False)
+        break
+else:
+    print(True)
