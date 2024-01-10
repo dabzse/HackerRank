09145ea5ceca32a95852153ce6fa1ee5013c4142
Mihaly Nyilas <dr.dabzse@gmail.com>
2024-01-10 15:10:30 +0100

    add: Python: Errors and Exceptions: Exceptions

---
+++ Python/Errors and Exceptions/Exceptions.py
@@ -0,0 +1,8 @@
+# language: pypy3
+
+for _ in range(int(input())):
+    a, b = input().split()
+    try:
+        print(int(a) // int(b))
+    except Exception as e:
+        print('Error Code: ' + str(e))
