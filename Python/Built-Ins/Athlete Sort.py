379682c62964ed2685d1da0fe8e3fcdbaf02d8bd
Mihaly Nyilas <dr.dabzse@gmail.com>
2024-01-17 21:36:08 +0100

    add: Python: Built-Ins: Athlete Sort

---
+++ Python/Built-Ins/Athlete Sort.py
@@ -0,0 +1,8 @@
+# language: pypy3
+
+n, m = map(int, input().split())
+rows = [input() for _ in range(n)]
+k = int(input())
+
+for row in sorted(rows, key=lambda row: int(row.split()[k])):
+    print(row)
