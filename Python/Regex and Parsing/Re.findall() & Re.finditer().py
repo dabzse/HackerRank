81a0c897a6867990dfe6dc89ab36d49ef701e672
Mihaly Nyilas <dr.dabzse@gmail.com>
2024-02-16 21:49:19 +0100

    add: Python: Regex and Parsing: Re.findall() & Re.finditer()

---
+++ Python/Regex and Parsing/Re.findall() & Re.finditer().py
@@ -0,0 +1,12 @@
+# language: pypy3
+
+import re
+
+vowels = "aeiou"
+consonants = "qwrtypsdfghjklzxcvbnm"
+match = re.findall(
+    r"(?<=[" + consonants + "])([" + vowels + "]{2,})(?=[" + consonants + "])",
+    input(),
+    flags=re.I,
+)
+print("\n".join(match or ["-1"]))
