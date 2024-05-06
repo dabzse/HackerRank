8c3667c1297202528959cdafee476e58b0cf0f77
Mihaly Nyilas <dr.dabzse@gmail.com>
2024-05-06 19:49:50 +0200

    add: Algorithms: Implementation: Day of the Programmer (php)

---
+++ Algorithms/Implementation/Day of the Programmer.php
@@ -0,0 +1,39 @@
+<?php
+
+/*
+ * Complete the 'dayOfProgrammer' function below.
+ *
+ * The function is expected to return a STRING.
+ * The function accepts INTEGER year as parameter.
+ */
+
+function dayOfProgrammer($year) {
+    # Write your code here
+
+    if($year == 1918)
+        return "26.09.1918";
+
+    if ($year < 1918) {
+        if ($year % 4 == 0)
+            return "12.09." . $year;
+        else
+            return "13.09." . $year;
+    }
+
+    if($year > 1918) {
+        if($year % 400 == 0 || ($year % 4 == 0 && $year % 100 != 0)) {
+            return "12.09." . $year;
+        }
+        else
+            return "13.09." . $year;
+    }
+}
+
+$fptr = fopen(getenv("OUTPUT_PATH"), "w");
+
+$year = intval(trim(fgets(STDIN)));
+
+$result = dayOfProgrammer($year);
+
+fwrite($fptr, $result . "\n");
+fclose($fptr);
