b1cb78f149b7c81b8043612110a04741352336eb
Mihaly Nyilas <dr.dabzse@gmail.com>
2024-05-07 16:13:25 +0200

    add: Algorithms: Implementation: Bill Division (c#)

---
+++ Algorithms/Implementation/Bill Division.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+class Result
+{
+
+    /*
+     * Complete the 'bonAppetit' function below.
+     *
+     * The function accepts following parameters:
+     *  1. INTEGER_ARRAY bill
+     *  2. INTEGER k
+     *  3. INTEGER b
+     */
+
+    public static void bonAppetit(List<int> bill, int k, int b)
+    {
+        int sum = 0;
+        for (int i = 0; i < bill.Count; i++)
+        {
+            if (i != k)
+                sum += bill[i];
+        }
+        if (sum / 2 == b)
+            Console.WriteLine("Bon Appetit");
+        else
+            Console.WriteLine(b - sum / 2);
+    }
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        string[] firstMultipleInput = Console.ReadLine().TrimEnd().Split(' ');
+
+        int n = Convert.ToInt32(firstMultipleInput[0]);
+        int k = Convert.ToInt32(firstMultipleInput[1]);
+
+        List<int> bill = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(billTemp => Convert.ToInt32(billTemp)).ToList();
+
+        int b = Convert.ToInt32(Console.ReadLine().Trim());
+
+        Result.bonAppetit(bill, k, b);
+    }
+}
