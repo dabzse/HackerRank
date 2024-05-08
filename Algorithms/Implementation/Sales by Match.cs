33279d80dad9621cd9571c224dc599148e5e9c98
Mihaly Nyilas <dr.dabzse@gmail.com>
2024-05-08 16:04:48 +0200

    add: Algorithms: Implementation: Sales by Match (c#)

---
+++ Algorithms/Implementation/Sales by Match.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+class Result
+{
+
+    /*
+     * Complete the 'sockMerchant' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER n
+     *  2. INTEGER_ARRAY ar
+     */
+
+    public static int sockMerchant(int n, List<int> ar)
+    {
+        int count = 0;
+        for (int i = 0; i < ar.Count; i++)
+        {
+            for (int j = i + 1; j < ar.Count; j++)
+            {
+                if (ar[i] == ar[j])
+                {
+                    count++;
+                    ar.RemoveAt(j);
+                    break;
+                }
+            }
+        }
+        return count;
+    }
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<int> ar = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arTemp => Convert.ToInt32(arTemp)).ToList();
+
+        int result = Result.sockMerchant(n, ar);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
