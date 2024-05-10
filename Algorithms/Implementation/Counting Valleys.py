c918ea92de485b827e96cb17dff6623054237334
Mihaly Nyilas <dr.dabzse@gmail.com>
2024-05-10 16:52:40 +0200

    add: Algorithms: Implementation: Counting Valleys (pypy3)

---
+++ Algorithms/Implementation/Counting Valleys.py
@@ -0,0 +1,34 @@
+import os
+
+#
+# Complete the 'countingValleys' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER steps
+#  2. STRING path
+#
+
+def countingValleys(steps, path):
+    # Write your code here
+    level = 0
+    count = 0
+    for i in path:
+        if i == 'U':
+            level += 1
+        else:
+            level -= 1
+        if level == 0 and i == 'U':
+            count += 1
+    return count
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    steps = int(input().strip())
+    path = input()
+
+    result = countingValleys(steps, path)
+
+    fptr.write(str(result) + '\n')
+    fptr.close()
