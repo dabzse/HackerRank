c24b272304f0bbee813f9681c16332d70aa76951
Mihaly Nyilas <dr.dabzse@gmail.com>
2024-05-12 17:29:04 +0200

    add: Algorithms: Implementation: Cats and a Mouse (c#)

---
+++ Algorithms/Implementation/Cats and a Mouse.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+class Solution {
+
+    // Complete the catAndMouse function below.
+    static string catAndMouse(int x, int y, int z)
+    {
+        if (Math.Abs(x - z) == Math.Abs(y - z))
+            return "Mouse C";
+        else if (Math.Abs(x - z) < Math.Abs(y - z))
+            return "Cat A";
+        else
+            return "Cat B";
+    }
+
+    static void Main(string[] args) {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int q = Convert.ToInt32(Console.ReadLine());
+
+        for (int qItr = 0; qItr < q; qItr++) {
+            string[] xyz = Console.ReadLine().Split(' ');
+            int x = Convert.ToInt32(xyz[0]);
+            int y = Convert.ToInt32(xyz[1]);
+            int z = Convert.ToInt32(xyz[2]);
+            string result = catAndMouse(x, y, z);
+            textWriter.WriteLine(result);
+        }
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
