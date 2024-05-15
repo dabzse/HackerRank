825d954cae5baef5b26120505013a44ecbfc7ac0
Mihaly Nyilas <dr.dabzse@gmail.com>
2024-05-15 18:39:32 +0200

    add: Algorithms: Implementation: Climbing the Leaderboard (c#)

---
+++ Algorithms/Implementation/Climbing the Leaderboard.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+class Result
+{
+
+    /*
+     * Complete the 'climbingLeaderboard' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts following parameters:
+     *  1. INTEGER_ARRAY ranked
+     *  2. INTEGER_ARRAY player
+     */
+
+    public static List<int> climbingLeaderboard(List<int> ranked, List<int> player)
+    {
+        List<int> result = new List<int>();
+        List<int> uniqueRanked = ranked.Distinct().ToList();
+        int rank = uniqueRanked.Count;
+        foreach(int score in player)
+        {
+            while(rank > 0 && uniqueRanked[rank - 1] <= score)
+                rank--;
+            result.Add(rank + 1);
+        }
+        return result;
+    }
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int rankedCount = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<int> ranked = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(rankedTemp => Convert.ToInt32(rankedTemp)).ToList();
+
+        int playerCount = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<int> player = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(playerTemp => Convert.ToInt32(playerTemp)).ToList();
+
+        List<int> result = Result.climbingLeaderboard(ranked, player);
+
+        textWriter.WriteLine(String.Join("\n", result));
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
