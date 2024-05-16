c3d9745ada66a9ce775f0be125b6bb81ef51b49a
Mihaly Nyilas <dr.dabzse@gmail.com>
2024-05-16 20:15:15 +0200

    add: Algorithms: Implementation: The Hurdle Race (c#)

---
+++ Algorithms/Implementation/The Hurdle Race.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+class Result
+{
+
+    /*
+     * Complete the 'hurdleRace' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER k
+     *  2. INTEGER_ARRAY height
+     */
+
+    public static int hurdleRace(int k, List<int> height)
+    {
+        int max = height.Max();
+        if(max <= k) return 0;
+        else return max - k;
+    }
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        string[] firstMultipleInput = Console.ReadLine().TrimEnd().Split(' ');
+        int n = Convert.ToInt32(firstMultipleInput[0]);
+        int k = Convert.ToInt32(firstMultipleInput[1]);
+
+        List<int> height = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(heightTemp => Convert.ToInt32(heightTemp)).ToList();
+
+        int result = Result.hurdleRace(k, height);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
