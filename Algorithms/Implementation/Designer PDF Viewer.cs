4a44dfa9a79644c14d6d9466ea95332061330d29
Mihaly Nyilas <dr.dabzse@gmail.com>
2024-05-17 21:10:14 +0200

    add: Algorithms: Implementation: Designer PDF Viewer (c#)

---
+++ Algorithms/Implementation/Designer PDF Viewer.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+class Result
+{
+
+    /*
+     * Complete the 'designerPdfViewer' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER_ARRAY h
+     *  2. STRING word
+     */
+
+    public static int designerPdfViewer(List<int> h, string word)
+    {
+        int max = 0;
+        foreach(char c in word)
+        {
+            if(h[c - 'a'] > max)
+            {
+                max = h[c - 'a'];
+            }
+        }
+        return max * word.Length;
+    }
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        List<int> h = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(hTemp => Convert.ToInt32(hTemp)).ToList();
+
+        string word = Console.ReadLine();
+        int result = Result.designerPdfViewer(h, word);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
