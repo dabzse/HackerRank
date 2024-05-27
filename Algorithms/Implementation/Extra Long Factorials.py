71c5e4b4087ba4ea954a9130531efa836a52fb26
Mihaly Nyilas <dr.dabzse@gmail.com>
2024-05-27 15:16:21 +0200

    add: Algorithms: Implementation: Extra Long Factorials (pypy3)

---
+++ Algorithms/Implementation/Extra Long Factorials.py
@@ -0,0 +1,18 @@
+#
+# Complete the 'extraLongFactorials' function below.
+#
+# The function accepts INTEGER n as parameter.
+#
+
+def extraLongFactorials(n):
+    # Write your code here
+    result = 1
+    for i in range(1, n+1):
+        result *= i
+    print(result)
+
+
+if __name__ == '__main__':
+    n = int(input().strip())
+
+    extraLongFactorials(n)
