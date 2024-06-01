42ce574a5c10f4c3d1eed0a814144f9061351362
Mihaly Nyilas <dr.dabzse@gmail.com>
2024-06-01 23:22:57 +0200

    add: Algorithms: Implementation: Repeated String (c#)

---
+++ Algorithms/Implementation/Repeated String.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+class Result
+{
+
+    /*
+     * Complete the 'repeatedString' function below.
+     *
+     * The function is expected to return a LONG_INTEGER.
+     * The function accepts following parameters:
+     *  1. STRING s
+     *  2. LONG_INTEGER n
+     */
+
+    public static long repeatedString(string s, long n)
+    {
+        long count = s.Count(c => c == 'a');
+        return count * (n / s.Length) + s.Substring(0, (int)(n % s.Length)).Count(c => c == 'a');
+    }
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        string s = Console.ReadLine();
+
+        long n = Convert.ToInt64(Console.ReadLine().Trim());
+
+        long result = Result.repeatedString(s, n);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
