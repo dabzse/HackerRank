609cc6274aa67d03e5c5ff31316e5120ab713875
Mihaly Nyilas <dr.dabzse@gmail.com>
2024-06-08 19:30:26 +0200

    add: Algorithms: Implementation: Organizing Containers of Balls (pypy3)

---
+++ Algorithms/Implementation/Organizing Containers of Balls.py
@@ -0,0 +1,39 @@
+import os
+
+#
+# Complete the 'organizingContainers' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts 2D_INTEGER_ARRAY container as parameter.
+#
+
+def organizingContainers(container):
+    # Write your code here
+    sum_row = [sum(row) for row in container]
+    sum_col = [sum(col) for col in zip(*container)]
+    sum_row.sort()
+    sum_col.sort()
+
+    if sum_row == sum_col:
+        return "Possible"
+    else:
+        return "Impossible"
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    q = int(input().strip())
+
+    for q_itr in range(q):
+        n = int(input().strip())
+
+        container = []
+
+        for _ in range(n):
+            container.append(list(map(int, input().rstrip().split())))
+
+        result = organizingContainers(container)
+
+        fptr.write(result + '\n')
+
+    fptr.close()
