768b9b2f6e4618864d42911ec8b28d01f150e1ef
Mihaly Nyilas <dr.dabzse@gmail.com>
2024-06-22 17:08:42 +0200

    add: Algorithms: Implementation: Manasa and Stones (pypy3)

---
+++ Algorithms/Implementation/Manasa and Stones.py
@@ -0,0 +1,36 @@
+import os
+
+#
+# Complete the 'stones' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. INTEGER a
+#  3. INTEGER b
+#
+
+def stones(n, a, b):
+    # Write your code here
+    result = set()
+    for i in range(n):
+        result.add(a * (n - 1 - i) + b * i)
+    return sorted(result)
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    T = int(input().strip())
+
+    for T_itr in range(T):
+        n = int(input().strip())
+        a = int(input().strip())
+        b = int(input().strip())
+
+        result = stones(n, a, b)
+
+        fptr.write(' '.join(map(str, result)))
+        fptr.write('\n')
+
+    fptr.close()
