13be3637a4e53da1d7e5d671227fa29d62ed0e4e
Mihaly Nyilas <dr.dabzse@gmail.com>
2024-06-25 20:35:36 +0200

    add: Algorithms: Implementation: Strange Counter (pypy3)

---
+++ Algorithms/Implementation/Strange Counter.py
@@ -0,0 +1,28 @@
+import os
+
+#
+# Complete the 'strangeCounter' function below.
+#
+# The function is expected to return a LONG_INTEGER.
+# The function accepts LONG_INTEGER t as parameter.
+#
+
+def strangeCounter(t):
+    # Write your code here
+    a = 3
+    while t > a:
+        t -= a
+        a *= 2
+    return a - t + 1
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input().strip())
+
+    result = strangeCounter(t)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
