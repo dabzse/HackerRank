ef015a35604be3133d03550d838afeb3ecef6e42
Mihaly Nyilas <dr.dabzse@gmail.com>
2024-07-02 18:29:09 +0200

    add: Algorithms: Implementation: Larry's Array (c#)

---
+++ Algorithms/Implementation/Larry's Array.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+class Result
+{
+
+    /*
+     * Complete the 'larrysArray' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts INTEGER_ARRAY A as parameter.
+     */
+
+    public static string larrysArray(List<int> A)
+    {
+        int n = A.Count;
+        int inversions = 0;
+
+        for (int i = 0; i < n - 1; i++)
+        {
+            for (int j = i + 1; j < n; j++)
+            {
+                if (A[i] > A[j])
+                {
+                    inversions++;
+                }
+            }
+        }
+
+        return (inversions % 2 == 0) ? "YES" : "NO";
+    }
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int t = Convert.ToInt32(Console.ReadLine().Trim());
+
+        for (int tItr = 0; tItr < t; tItr++)
+        {
+            int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+            List<int> A = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(ATemp => Convert.ToInt32(ATemp)).ToList();
+
+            string result = Result.larrysArray(A);
+
+            textWriter.WriteLine(result);
+        }
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
