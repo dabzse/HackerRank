0fb093436be235c483841cdeddf843f2847155d9
Mihaly Nyilas <dr.dabzse@gmail.com>
2024-07-09 22:09:36 +0200

    add: Algorithms: Strings: HackerRank in a String! (pypy3)

---
+++ Algorithms/Strings/HackerRank in a String!.py
@@ -0,0 +1,34 @@
+import os
+
+#
+# Complete the 'hackerrankInString' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts STRING s as parameter.
+#
+
+def hackerrankInString(s):
+    # Write your code here
+    hackerrank = "hackerrank"
+    i = 0
+    for c in s:
+        if c == hackerrank[i]:
+            i += 1
+            if i == len(hackerrank):
+                return "YES"
+    return "NO"
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    q = int(input().strip())
+
+    for q_itr in range(q):
+        s = input()
+
+        result = hackerrankInString(s)
+
+        fptr.write(result + '\n')
+
+    fptr.close()
