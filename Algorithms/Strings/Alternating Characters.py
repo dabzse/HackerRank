e124a19635f918312077d73654a850235d94eac1
Mihaly Nyilas <dr.dabzse@gmail.com>
2024-07-15 19:10:43 +0200

    add: Algorithms: Strings: Alternating Characters (pypy3)

---
+++ Algorithms/Strings/Alternating Characters.py
@@ -0,0 +1,34 @@
+import os
+
+#
+# Complete the 'alternatingCharacters' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts STRING s as parameter.
+#
+
+def alternatingCharacters(s):
+    # Write your code here
+    count = 0
+    prev = s[0]
+    for curr in s[1:]:
+        if curr == prev:
+            count += 1
+        else:
+            prev = curr
+    return count
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    q = int(input().strip())
+
+    for q_itr in range(q):
+        s = input()
+
+        result = alternatingCharacters(s)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
