a265fde5ca6ba0c2f6c4879ef34ffd0b5a7db82d
Mihaly Nyilas <dr.dabzse@gmail.com>
2024-07-22 20:54:51 +0200

    add: Algorithms: Strings: Two Strings (c#)

---
+++ Algorithms/Strings/Two Strings.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+class Result
+{
+
+    /*
+     * Complete the 'twoStrings' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts following parameters:
+     *  1. STRING s1
+     *  2. STRING s2
+     */
+
+    public static string twoStrings(string s1, string s2)
+    {
+        var hash = new HashSet<char>(s1.ToLower());
+        foreach (var c in s2.ToLower())
+        {
+            if (hash.Contains(c))
+            {
+                return "YES";
+            }
+        }
+        return "NO";
+    }
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int q = Convert.ToInt32(Console.ReadLine().Trim());
+
+        for (int qItr = 0; qItr < q; qItr++)
+        {
+            string s1 = Console.ReadLine();
+            string s2 = Console.ReadLine();
+
+            string result = Result.twoStrings(s1, s2);
+
+            textWriter.WriteLine(result);
+        }
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
