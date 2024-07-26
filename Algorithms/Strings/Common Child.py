2895d6b77f2f2674898fb1b0a390965a46a6cd9e
Mihaly Nyilas <dr.dabzse@gmail.com>
2024-07-26 21:23:30 +0200

    add: Algorithms: Strings: Common Child (pypy3)

---
+++ Algorithms/Strings/Common Child.py
@@ -0,0 +1,36 @@
+import os
+
+#
+# Complete the 'commonChild' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. STRING s1
+#  2. STRING s2
+#
+
+def commonChild(s1, s2):
+    # Write your code here
+    len1, len2 = len(s1), len(s2)
+    dp = [[0] * (len2 + 1) for _ in range(len1 + 1)]
+
+    for i in range(1, len1 + 1):
+        for j in range(1, len2 + 1):
+            if s1[i - 1] == s2[j - 1]:
+                dp[i][j] = dp[i - 1][j - 1] + 1
+            else:
+                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+
+    return dp[len1][len2]
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s1 = input()
+    s2 = input()
+
+    result = commonChild(s1, s2)
+
+    fptr.write(str(result) + '\n')
+    fptr.close()
