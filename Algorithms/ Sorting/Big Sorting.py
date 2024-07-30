b74552855c74d89f8710af95f06eea6b84fc28f8
Mihaly Nyilas <dr.dabzse@gmail.com>
2024-07-30 21:30:54 +0200

    add: Algorithms: Sorting: Big Sorting (pypy3)

---
+++ Algorithms/ Sorting/Big Sorting.py
@@ -0,0 +1,31 @@
+import os
+
+#
+# Complete the 'bigSorting' function below.
+#
+# The function is expected to return a STRING_ARRAY.
+# The function accepts STRING_ARRAY unsorted as parameter.
+#
+
+def bigSorting(unsorted):
+    # Write your code here
+    return sorted(unsorted, key=lambda x: (len(x), x))
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    unsorted = []
+
+    for _ in range(n):
+        unsorted_item = input()
+        unsorted.append(unsorted_item)
+
+    result = bigSorting(unsorted)
+
+    fptr.write('\n'.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
