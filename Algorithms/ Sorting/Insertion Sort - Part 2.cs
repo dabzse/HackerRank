3ef28826357e99da441a7888a809b61ba76da15e
Mihaly Nyilas <dr.dabzse@gmail.com>
2024-08-02 17:03:04 +0200

    add: Algorithms: Sorting: Insertion Sort - Part 2 (c#)

---
+++ Algorithms/ Sorting/Insertion Sort - Part 2.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+class Result
+{
+
+    /*
+     * Complete the 'insertionSort2' function below.
+     *
+     * The function accepts following parameters:
+     *  1. INTEGER n
+     *  2. INTEGER_ARRAY arr
+     */
+
+    public static void insertionSort2(int n, List<int> arr)
+    {
+        for (int i = 1; i < n; i++)
+        {
+            int key = arr[i];
+            int j = i - 1;
+            while (j >= 0 && arr[j] > key)
+            {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+            arr[j + 1] = key;
+            Console.WriteLine(string.Join(" ", arr));
+        }
+    }
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+
+        Result.insertionSort2(n, arr);
+    }
+}
