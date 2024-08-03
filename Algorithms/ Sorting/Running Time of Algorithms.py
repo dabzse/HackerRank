c0fceab144bc22763f9206bd2ad4717b1c3e17bd
Mihaly Nyilas <dr.dabzse@gmail.com>
2024-08-03 20:12:01 +0200

    add: Algorithms: Sorting: Running Time of Algorithms (pypy3)

---
+++ Algorithms/ Sorting/Running Time of Algorithms.py
@@ -0,0 +1,34 @@
+import os
+
+#
+# Complete the 'runningTime' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def runningTime(arr):
+    # Write your code here
+    count = 0
+    for i in range(1, len(arr)):
+        key = arr[i]
+        j = i - 1
+        while j >= 0 and arr[j] > key:
+            arr[j + 1] = arr[j]
+            j -= 1
+            count += 1
+        arr[j + 1] = key
+    return count
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = runningTime(arr)
+
+    fptr.write(str(result) + '\n')
+    fptr.close()
