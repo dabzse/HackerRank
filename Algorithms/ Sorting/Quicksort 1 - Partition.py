ccec2508a175b7fad870be9895a549a8296e0158
Mihaly Nyilas <dr.dabzse@gmail.com>
2024-08-04 21:33:32 +0200

    add: Algorithms: Sorting: Quicksort 1 - Partition (pypy3)

---
+++ Algorithms/ Sorting/Quicksort 1 - Partition.py
@@ -0,0 +1,32 @@
+import os
+
+
+#
+# Complete the 'quickSort' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def quickSort(arr):
+    # Write your code here
+    if len(arr) <= 1:
+        return arr
+    pivot = arr[0]
+    less = [x for x in arr[1:] if x <= pivot]
+    greater = [x for x in arr[1:] if x > pivot]
+    return quickSort(less) + [pivot] + quickSort(greater)
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = quickSort(arr)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+    fptr.close()
