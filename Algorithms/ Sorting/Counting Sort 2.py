2772c2796c23ee876690a17acd52aa28a70ffab1
Mihaly Nyilas <dr.dabzse@gmail.com>
2024-08-07 17:57:20 +0200

    add: Algorithms: Sorting: Counting Sort 2 (pypy3)

---
+++ Algorithms/ Sorting/Counting Sort 2.py
@@ -0,0 +1,34 @@
+import os
+
+#
+# Complete the 'countingSort' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def countingSort(arr):
+    # Write your code here
+    count = [0] * (100 + 1)
+    for num in arr:
+        count[num] += 1
+
+    sorted_arr = []
+    for i, val in enumerate(count):
+        sorted_arr += [i] * val
+
+    return sorted_arr
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = countingSort(arr)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+    fptr.close()
