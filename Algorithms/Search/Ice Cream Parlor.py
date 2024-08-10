5fe811331452f9697a4c074033bbe421b91667a9
Mihaly Nyilas <dr.dabzse@gmail.com>
2024-08-10 21:19:02 +0200

    add: Algorithms: Search: Ice Cream Parlor (pypy3)

---
+++ Algorithms/Search/Ice Cream Parlor.py
@@ -0,0 +1,36 @@
+import os
+
+#
+# Complete the 'icecreamParlor' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts following parameters:
+#  1. INTEGER m
+#  2. INTEGER_ARRAY arr
+#
+
+def icecreamParlor(m, arr):
+    # Write your code here
+    for i, v1 in enumerate(arr):
+        for j, v2 in enumerate(arr[i+1:], i+1):
+            if v1 + v2 == m:
+                return [i+1, j+1]
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input().strip())
+
+    for t_itr in range(t):
+        m = int(input().strip())
+        n = int(input().strip())
+
+        arr = list(map(int, input().rstrip().split()))
+
+        result = icecreamParlor(m, arr)
+
+        fptr.write(' '.join(map(str, result)))
+        fptr.write('\n')
+
+    fptr.close()
