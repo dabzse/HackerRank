8e978412168a18fcb1c00754e5308a982ecb6177
Mihaly Nyilas <dr.dabzse@gmail.com>
2024-08-14 22:28:33 +0200

    add: Algorithms: Search: Sherlock and Array (pypy3)

---
+++ Algorithms/Search/Sherlock and Array.py
@@ -0,0 +1,36 @@
+import os
+
+#
+# Complete the 'balancedSums' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def balancedSums(arr):
+    # Write your code here
+    total = sum(arr)
+    left = 0
+    for i, num in enumerate(arr):
+        total -= num
+        if left == total:
+            return "YES"
+        left += num
+    return "NO"
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    T = int(input().strip())
+
+    for T_itr in range(T):
+        n = int(input().strip())
+
+        arr = list(map(int, input().rstrip().split()))
+
+        result = balancedSums(arr)
+
+        fptr.write(result + '\n')
+
+    fptr.close()
