75da43eb8c839d2dc6fc24f77d1c465c7a869970
Mihaly Nyilas <dr.dabzse@gmail.com>
2024-08-18 21:46:31 +0200

    add: Algorithms: Greedy: Marc's Cakewalk (pypy3)

---
+++ Algorithms/Greedy/Marc's Cakewalk.py
@@ -0,0 +1,29 @@
+import os
+
+#
+# Complete the 'marcsCakewalk' function below.
+#
+# The function is expected to return a LONG_INTEGER.
+# The function accepts INTEGER_ARRAY calorie as parameter.
+#
+
+def marcsCakewalk(calorie):
+    # Write your code here
+    calorie.sort(reverse = True)
+    total = 0
+    for i in range(len(calorie)):
+        total += calorie[i] * 2**i
+    return total
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    calorie = list(map(int, input().rstrip().split()))
+
+    result = marcsCakewalk(calorie)
+
+    fptr.write(str(result) + '\n')
+    fptr.close()
