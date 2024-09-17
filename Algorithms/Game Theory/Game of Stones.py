b9fed730a053df627bcdafde3983c9dd532d65d0
Mihaly Nyilas <dr.dabzse@gmail.com>
2024-09-17 23:19:18 +0200

    add: Algorithms: Game Theory: Game of Stones (pypy3)

---
+++ Algorithms/Game Theory/Game of Stones.py
@@ -0,0 +1,30 @@
+import os
+
+#
+# Complete the 'gameOfStones' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts INTEGER n as parameter.
+#
+
+def gameOfStones(n):
+    # Write your code here
+    if n % 7 in [0, 1]:
+        return "Second"
+    else:
+        return "First"
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input().strip())
+
+    for t_itr in range(t):
+        n = int(input().strip())
+
+        result = gameOfStones(n)
+
+        fptr.write(result + '\n')
+
+    fptr.close()
