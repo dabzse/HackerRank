f658f1be458f56aec8aba93dd64df282d7991925
Mihaly Nyilas <dr.dabzse@gmail.com>
2024-10-06 20:57:20 +0200

    add: Algorithms: Graph Theory: Kruskal (MST): Really Special Subtree (pypy3)

---
+++ Algorithms/Graph Theory/Kruskal (MST): Really Special Subtree.py
@@ -0,0 +1,81 @@
+import os
+
+#
+# Complete the 'kruskals' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts WEIGHTED_INTEGER_GRAPH g as parameter.
+#
+
+#
+# For the weighted graph, <name>:
+#
+# 1. The number of nodes is <name>_nodes.
+# 2. The number of edges is <name>_edges.
+# 3. An edge exists between <name>_from[i] and <name>_to[i]. The weight of the edge is <name>_weight[i].
+#
+#
+
+class UnionFind:
+    def __init__(self, n):
+        self.parent = list(range(n))
+        self.rank = [0] * n
+
+    def find(self, u):
+        if self.parent[u] != u:
+            self.parent[u] = self.find(self.parent[u])
+        return self.parent[u]
+
+    def union(self, u, v):
+        root_u = self.find(u)
+        root_v = self.find(v)
+        if root_u != root_v:
+            if self.rank[root_u] > self.rank[root_v]:
+                self.parent[root_v] = root_u
+            elif self.rank[root_u] < self.rank[root_v]:
+                self.parent[root_u] = root_v
+            else:
+                self.parent[root_v] = root_u
+                self.rank[root_u] += 1
+
+def kruskals(g_nodes, g_from, g_to, g_weight):
+    # Write your code here
+    edges = []
+
+    for i, (u, v, weight) in enumerate(zip(g_from, g_to, g_weight)):
+        edges.append((weight, u - 1, v - 1))
+
+    edges.sort()
+
+    uf = UnionFind(g_nodes)
+    mst_weight = 0
+    mst_edges = 0
+
+    for weight, u, v in edges:
+        if uf.find(u) != uf.find(v):
+            uf.union(u, v)
+            mst_weight += weight
+            mst_edges += 1
+            if mst_edges == g_nodes - 1:
+                break
+
+    return mst_weight
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    g_nodes, g_edges = map(int, input().rstrip().split())
+
+    g_from = [0] * g_edges
+    g_to = [0] * g_edges
+    g_weight = [0] * g_edges
+
+    for i in range(g_edges):
+        g_from[i], g_to[i], g_weight[i] = map(int, input().rstrip().split())
+
+    res = kruskals(g_nodes, g_from, g_to, g_weight)
+
+    # Write your code here.
+    fptr.write(str(res) + "\n")
+    fptr.close()
